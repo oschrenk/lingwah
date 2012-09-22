13aa2b7bce4f17d1952d7014668b0076621870a3
Oliver Schrenk <oliver.schrenk@gmail.com>
2012-09-23 01:29:02 +0200

    Organize imports

--- src/main/java/com/googlecode/lingwah/parser/TerminalParser.java
+++ src/main/java/com/googlecode/lingwah/parser/TerminalParser.java
@@ -3,8 +3,6 @@ package com.googlecode.lingwah.parser;
 import java.util.Collections;
 import java.util.List;
 
-import com.googlecode.lingwah.ParseContext;
-import com.googlecode.lingwah.ParseResults;
 import com.googlecode.lingwah.Parser;
 
 /**
@@ -17,7 +15,7 @@ abstract public class TerminalParser extends Parser {
 	public List<Parser> getDependencies() {
 		return Collections.<Parser>emptyList();
 	}
-	
+
 	@Override
 	public boolean isRecursive() {
 		return false;
