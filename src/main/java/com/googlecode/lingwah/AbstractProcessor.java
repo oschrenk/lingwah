3a82ae2099f0919f05532ab8e8234281f85cdd11
Oliver Schrenk <oliver.schrenk@gmail.com>
2012-09-23 05:04:44 +0200

    Try to fix stackoverflow.com/q/5666027/

--- src/main/java/com/googlecode/lingwah/AbstractProcessor.java
+++ src/main/java/com/googlecode/lingwah/AbstractProcessor.java
@@ -14,91 +14,95 @@ import com.googlecode.lingwah.util.ProcessorUtils;
  */
 abstract public class AbstractProcessor implements MatchProcessor {
 
-	private HashMap<Match, Object> _results= new HashMap<Match, Object>();
-	private HashSet<Match> _visited= new HashSet<Match>();
-	private HashSet<Match> _completed= new HashSet<Match>();
+	private final HashMap<Match, Object> _results= new HashMap<Match, Object>();
+	private final HashSet<Match> _visited= new HashSet<Match>();
+	private final HashSet<Match> _completed= new HashSet<Match>();
 	private Match _currentMatch;
-	
-	
-	public boolean process(Match node) {
 
-		if (_visited.contains(node))
+
+	@Override
+	public boolean process(final Match node) {
+
+		if (_visited.contains(node)) {
 			return true;
+		}
 		_visited.add(node);
-		Match prevMatch= _currentMatch;
+		final Match prevMatch= _currentMatch;
 		_currentMatch= node;
-		Method visitMethod = ProcessorUtils.findVisitMethod(this, node);
+		final Method visitMethod = ProcessorUtils.findVisitMethod(this, node);
 		try {
-			Boolean visitChildren= (Boolean) visitMethod.invoke(this, new Object[] { node });
+			final Boolean visitChildren= (Boolean) visitMethod.invoke(this, new Object[] { node });
 			return visitChildren;
-			
-		} catch (IllegalArgumentException e) {
+
+		} catch (final IllegalArgumentException e) {
 			throw new RuntimeException("Error invoking method '"+visitMethod.getName()+"'", e);
-		} catch (IllegalAccessException e) {
+		} catch (final IllegalAccessException e) {
 			throw new RuntimeException("Error invoking method '"+visitMethod.getName()+"'", e);
-		} catch (InvocationTargetException e) {
+		} catch (final InvocationTargetException e) {
 			throw new RuntimeException("Error invoking method '"+visitMethod.getName()+"'", e);
 		}
 		finally {
 			_currentMatch= prevMatch;
 		}
 	}
-	
+
 	/**
-	 * Invoked after invoking the visit method and visiting all children nodes   
+	 * Invoked after invoking the visit method and visiting all children nodes
 	 */
-	public void complete(Match node) {
-		if (_completed.contains(node))
+	@Override
+	public void complete(final Match node) {
+		if (_completed.contains(node)) {
 			return;
+		}
 		_completed.add(node);
 
-		Match prevMatch= _currentMatch;
+		final Match prevMatch= _currentMatch;
 		_currentMatch= node;
-		Method leaveMethod = ProcessorUtils.findLeaveMethod(this, node);
+		final Method leaveMethod = ProcessorUtils.findLeaveMethod(this, node);
 		try {
 			leaveMethod.invoke(this, new Object[] { node });
-		} catch (IllegalArgumentException e) {
+		} catch (final IllegalArgumentException e) {
 			throw new RuntimeException("Error invoking method '"+leaveMethod.getName()+"'", e);
-		} catch (IllegalAccessException e) {
+		} catch (final IllegalAccessException e) {
 			throw new RuntimeException("Error invoking method '"+leaveMethod.getName()+"'", e);
-		} catch (InvocationTargetException e) {
+		} catch (final InvocationTargetException e) {
 			throw new RuntimeException("Error invoking method '"+leaveMethod.getName()+"'", e);
 		}
 		finally {
 			_currentMatch= prevMatch;
 		}
 	}
-	
+
 	public Match getCurrentMatch() {
 		return _currentMatch;
 	}
 
 	/**
-	 * Save result for the Match that is currently being processed 
+	 * Save result for the Match that is currently being processed
 	 */
-	public void putResult(Object result) {
+	public void putResult(final Object result) {
 		putResult(getCurrentMatch(), result);
-		
+
 	}
 
-	public void putResult(Match match, Object result) {
+	public void putResult(final Match match, final Object result) {
 		_results.put(match, result);
-		
+
 	}
 
 	/**
 	 * Get the result for the given Match.
-	 * If there is no result and the Match has not yet been processed 
-	 * then the Match will be processed before returning. 
+	 * If there is no result and the Match has not yet been processed
+	 * then the Match will be processed before returning.
 	 */
 	@SuppressWarnings("unchecked")
-	public <T> T getResult(Match match) {
+	public Object getResult(final Match match) {
 		Object result= _results.get(match);
 		if (result == null && !_visited.contains(match)) {
 			match.accept(this);
 			result= _results.get(match);
 		}
-		return (T) result;
+		return result;
 	}
 
 
--- src/test/java/com/googlecode/lingwah/test/CalculatorProcessor.java
+++ src/test/java/com/googlecode/lingwah/test/CalculatorProcessor.java
@@ -11,48 +11,56 @@ import com.googlecode.lingwah.annotations.Processes;
 
 @Processes(CalculatorGrammar.class)
 public class CalculatorProcessor extends AbstractProcessor {
-	
-	static final CalculatorGrammar grammar= CalculatorGrammar.INSTANCE;
-	
-	public void completeAddition(Match expr) {
-		List<Match> children= expr.getChildrenByType(grammar.expr);
-		BigDecimal left= getResult(children.get(0));
-		BigDecimal right= getResult(children.get(1));
+
+	static final CalculatorGrammar grammar = CalculatorGrammar.INSTANCE;
+
+	public void completeAddition(final Match expr) {
+		final List<Match> children = expr.getChildrenByType(grammar.expr);
+		final BigDecimal left = (BigDecimal) getResult(children.get(0));
+		final BigDecimal right = (BigDecimal) getResult(children.get(1));
 		putResult(left.add(right));
 	}
-	public void completeSubtraction(Match expr) {
-		List<Match> children= expr.getChildrenByType(grammar.expr);
-		BigDecimal left= getResult(children.get(0));
-		BigDecimal right= getResult(children.get(1));
+
+	public void completeSubtraction(final Match expr) {
+		final List<Match> children = expr.getChildrenByType(grammar.expr);
+		final BigDecimal left = (BigDecimal) getResult(children.get(0));
+		final BigDecimal right = (BigDecimal) getResult(children.get(1));
 		putResult(left.subtract(right));
 	}
-	public void completeMultiplication(Match expr) {
-		List<Match> children= expr.getChildrenByType(grammar.expr);
-		BigDecimal left= getResult(children.get(0));
-		BigDecimal right= getResult(children.get(1));
+
+	public void completeMultiplication(final Match expr) {
+		final List<Match> children = expr.getChildrenByType(grammar.expr);
+		final BigDecimal left = (BigDecimal) getResult(children.get(0));
+		final BigDecimal right = (BigDecimal) getResult(children.get(1));
 		putResult(left.multiply(right));
 	}
-	public void completeDivision(Match expr) {
-		List<Match> children= expr.getChildrenByType(grammar.expr);
-		BigDecimal left= getResult(children.get(0));
-		BigDecimal right= getResult(children.get(1));
+
+	public void completeDivision(final Match expr) {
+		final List<Match> children = expr.getChildrenByType(grammar.expr);
+		final BigDecimal left = (BigDecimal) getResult(children.get(0));
+		final BigDecimal right = (BigDecimal) getResult(children.get(1));
 		putResult(left.divide(right, 28, RoundingMode.HALF_UP));
 	}
-	public void completeOperator(Match op) {
+
+	public void completeOperator(final Match op) {
 		putResult(getResult(op.getChildren().get(0)));
 	}
-	public void completeGroup(Match expr) {
+
+	public void completeGroup(final Match expr) {
 		putResult(getResult(expr.getChildByType(grammar.expr)));
 	}
-	public void completeDecimal(Match expr) {
+
+	public void completeDecimal(final Match expr) {
 		putResult(new BigDecimal(expr.getText()));
 	}
-	public void completeExpr(Match expr) {
+
+	public void completeExpr(final Match expr) {
 		putResult(getResult(expr.getChildren().get(0)));
 	}
 
-	public static BigDecimal process(ParseResults results) {
-		return new CalculatorProcessor().getResult(results.getLongestMatch());
+	public static BigDecimal process(final ParseResults results) {
+		return (BigDecimal) new CalculatorProcessor().getResult(results
+				.getLongestMatch());
 	}
 
 }
